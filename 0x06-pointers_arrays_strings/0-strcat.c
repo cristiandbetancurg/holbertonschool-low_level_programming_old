fd8f17d9d439b2182025ff27af29fa48d01a71b6
cristiandbetancurg <cristiandbetancurg@gmail.com>
2020-06-25 13:31:51 -0400

    Try tree

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,17 @@
 
 char *_strcat(char *dest, char *src)
 {
-	while (*dest != '\0')
-	{
-		dest++;
-	}
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-	return (dest);
+int i = 0;
+int j = 0;
+while (dest[i])
+{
+i++;
+}
+while (src[j])
+{
+dest[i + j] = src[j];
+j++;
+}
+
+return (dest);
 }
