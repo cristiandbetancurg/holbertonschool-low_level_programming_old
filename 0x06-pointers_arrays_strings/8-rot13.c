cb610b9f00abde7a703a0053557d80be53b013a8
cristiandbetancurg <cristiandbetancurg@gmail.com>
2020-06-25 20:30:12 -0400

    rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * rot13 -- Rot13
+ * @a: Array
+ * Return: rot13
+ */
+
+char *rot13(char *a)
+{
+	int j, i;
+	char b1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b2[] = "nopqrstuvwxyzabcdefghijklmnOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; b1[j] != '\0'; j++)
+		{
+			if (a[i] == b1[j])
+			{
+				a[i] = b2[j];
+				break
+			}
+		}
+	}
+
+
+	return (0);
+}
