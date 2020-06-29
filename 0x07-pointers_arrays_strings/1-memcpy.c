93fccf710d43fc910b0561589de6066acd848b68
cristiandbetancurg <cristiandbetancurg@gmail.com>
2020-06-29 17:40:36 -0400

    Update Style try 3

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
+
 /**
- * _memcpy copy memory area
- * @dest: The Pointer to detination memory
- * @src: The Pointer to source all memory
- * @n: First n bytes to Copy
- * Return: Pointer dest'
+ * _memcpy - function Copy memory Area
+ * @dest: pointer to destination memory
+ * @src: pointer to src memory
+ * @n: n bytes to copy
+ * Return: Pointer dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
