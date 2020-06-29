99623810564ae46d67aab33c53429c8d4201bec6
cristiandbetancurg <cristiandbetancurg@gmail.com>
2020-06-29 16:01:13 -0400

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _memcpy copy memory area
+ * @dest: The *Pointer to detination memory
+ * @src: The *Pointer to source all memory
+ * @n: First n bytes to Copy
+ * Return: Pointer dest'
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
