4384816d17c485f12f1953c51b1d3d5228479ba9
cristiandbetancurg <cristiandbetancurg@gmail.com>
2020-07-07 16:59:39 -0400

    strdup try 3

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,24 +1,24 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * _strdup - copy string now
- * @str: string to copy
+ * _strdup - copy of the string a string
+ * @str: string to the copy
  * Return: char
 */
 char *_strdup(char *str)
 {
 	int i = 0, j;
-	char *a;
+	char *c;
 
 	if (str == NULL)
 		return (NULL);
 	while (str[i] != '\0')
 		i++;
 	i = i + 1;
-	a = malloc(i * sizeof(char));
-	if (a == NULL)
+	c = malloc(i * sizeof(char));
+	if (c == NULL)
 		return (NULL);
 	for (j = 0; j < i; j++)
 		c[j] = str[j];
-	return (a);
+	return (c);
 }
